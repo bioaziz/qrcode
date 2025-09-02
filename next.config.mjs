9d119f2bf772aa6a0bd46387f7e20c9ee1c5bbc4
aziz <bgbedourorou@gmail.com>
2025-09-02 21:36:05 +0100

    dont fail build on lint

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@ import nextI18NextConfig from './next-i18next.config.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  i18n: nextI18NextConfig.i18n,
+    reactStrictMode: true,
+    i18n: nextI18NextConfig.i18n,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
